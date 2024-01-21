58d5dcf5215e3a10b8a4d605891a10919cd1cd19
satiwari26 <tiwarisaumitra26@gmail.com>
2024-01-20 19:18:51 -0800

    INtegrated the user updated information in the link, added further features in the application. Need to test if it runs without data.json file

--- BikerBot/botUp.js
+++ BikerBot/botUp.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const fs = require('fs');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.Google_Bard_API);
+const axios = require('axios');
 // const { joinVoiceChannel, createAudioResource, StreamType, createAudioPlayer, AudioPlayerStatus } = require('@discordjs/voice');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
@@ -14,57 +15,8 @@ app.listen(3000, ()=>{
     console.log("Bot is running");
 });
 
-app.get("/", (req,res)=>{
-    res.send("hello world! This is Bikers_Bot.");
-})
-
-client.on('messageCreate', async message => {
-    let mainResponse = "";
-    if(message.author.bot) return;  //to avoid making it respond to itself
-    if( message.content !== ''){
-    }   
-        console.log(message.content);
-        let promptVal = message.content.substring(21, message.content.length);
-        let responsePrompt = "";
-        if(promptVal.length > 0){   //only pass the value to bard if it's not null
-            responsePrompt = await run(promptVal);
-        }
-        if(responsePrompt == undefined){
-            mainResponse = "Sorry something went wrong!";
-        }
-        if(responsePrompt.length > 1500){
-            for(let i = 0; i < responsePrompt.length; i+=1500){
-                mainResponse = responsePrompt.substring(i, Math.min(i+1500,responsePrompt.length));
-                message.reply(mainResponse);
-            }
-        }
-        else if(responsePrompt !== ""){
-            try{
-                message.reply(responsePrompt);
-            }
-            catch(error){
-                console.log(error);
-                message.reply("Sorry something went wrong!");
-            }
-        }
-        else{
-            message.reply("Sorry something went wrong!");
-        }
-});
-
-
-
-async function run(prompt) {
-    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
-    return text;
-  }
-
 //storing the information fetch from the user's HandGesture
-class bikersInformation{
+class BikersInformation{
     constructor(){  //creates the fields to store the information
         this.userUpdateData = [];
         this.status = false;
@@ -82,5 +34,127 @@ class bikersInformation{
         }
     }
 }
+
+async function getLocation(lat, long) {
+    let response;
+    if(lat == 0 && long == 0){  //base condition to exit the function
+        return "No location found";
+    }
+
+    try{
+        response = await axios.get(`http://dev.virtualearth.net/REST/v1/Locations/${lat},${long}?o=json&key=${process.env.Bing_Map_API}`);
+    }
+    catch(error){
+        console.log(error);
+    }
+    let results;
+    try{
+        results = response.data.resourceSets[0].resources;
+    }
+    catch(error){
+        console.log(error);
+    }
+
+    if (results[0]) {
+        return results[0].address.formattedAddress;
+    } else {
+        return 'No results found';
+    }
+}
+
+// getLocation(35.2828, -120.6596)
+//     .then(address => console.log(address))
+//     .catch(error => console.error(error));
+
+async function getParseData(){
+    // parsing the location data for the user to get the message update
+    let parseData = [];
+    let bikersInformation = new BikersInformation();
+    bikersInformation.fetchInfo();
+
+    if(bikersInformation.status == true){
+        let locationCoordinates = [];
+        for(let i = 0; i < bikersInformation.userUpdateData.length; i++){
+            locationCoordinates.push(bikersInformation.userUpdateData[i].location);
+
+            let addr = await getLocation(bikersInformation.userUpdateData[i].location.lat,
+                bikersInformation.userUpdateData[i].location.long);
+
+            if(addr !== "No location found"){
+                parseData.push({date: bikersInformation.userUpdateData[i].date,reason: bikersInformation.userUpdateData[i].reason,
+                    name: bikersInformation.userUpdateData[i].userName, location: addr, 
+                    time: bikersInformation.userUpdateData[i].time});
+            }
+        }
+        app.get("/UpdateInfo", (req,res)=>{
+            res.send(locationCoordinates);
+        });
+    }
+    else{
+        app.get("/UpdateInfo", (req,res)=>{
+            res.send(locationCoordinates.push({lat:0,long:0}));
+        });
+    }
+    return parseData;
+}
+
+client.on('messageCreate', async message => {
+    //getting the prominant string ready for LLM
+    let parseData = await getParseData();
+    let prominantString = "";
+    // prominantString = "Consider the following information provided by other biking users in slo when responding to next prompt. \n";
+    for(let i = 0; i < parseData.length; i++){
+        prominantString += `**Date Posted**:  ${parseData[i].date},  **Update reason**:   ${parseData[i].reason},   **User's Name**:   ${parseData[i].name},   **location**:   ${parseData[i].location},   **Time Posted**:   ${parseData[i].time} \n`;
+    }
+
+    let mainResponse = "";
+    if(message.author.bot) return;  //to avoid making it respond to itself
+    if( message.content !== ''){
+    }   
+        console.log(message.content);
+        let promptVal = message.content.substring(21, message.content.length);
+        if(promptVal.toLowerCase().includes("update")){
+            message.reply(prominantString);
+        }
+        else{
+            let responsePrompt = "";
+            if(promptVal.length > 0){   //only pass the value to bard if it's not null
+                console.log(prominantString);
+                responsePrompt = await run(promptVal);
+            }
+            if(responsePrompt == undefined){
+                mainResponse = "Sorry something went wrong!";
+            }
+            if(responsePrompt.length > 1500){
+                for(let i = 0; i < responsePrompt.length; i+=1500){
+                    mainResponse = responsePrompt.substring(i, Math.min(i+1500,responsePrompt.length));
+                    message.reply(mainResponse);
+                }
+            }
+            else if(responsePrompt !== ""){
+                try{
+                    message.reply(responsePrompt);
+                }
+                catch(error){
+                    console.log(error);
+                    message.reply("Sorry something went wrong!");
+                }
+            }
+            else{
+                message.reply("Sorry something went wrong!");
+            }
+        }
+});
+
+
+
+async function run(prompt) {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+    return text;
+  }
 
 client.login(discord_token);
